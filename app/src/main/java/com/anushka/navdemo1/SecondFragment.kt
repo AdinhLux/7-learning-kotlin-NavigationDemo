fba699484f0f7370daa9b3ffc97c39d808f8d6e8
ADI <adinh@jiway.lu>
2021-09-13 21:41:55 +0200

    Second fragment : data binding

--- app/src/main/java/com/anushka/navdemo1/SecondFragment.kt
+++ app/src/main/java/com/anushka/navdemo1/SecondFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.anushka.navdemo1.databinding.FragmentSecondBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -17,6 +19,8 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class SecondFragment : Fragment() {
+    private lateinit var binding: FragmentSecondBinding
+
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -32,9 +36,10 @@ class SecondFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_second, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_second, container, false)
+        return binding.root
     }
 
     companion object {
